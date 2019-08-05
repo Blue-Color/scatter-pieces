a4b2cb808deb9a8624077ce38b91a2f38e6440f6
FeiBlueColor <50509043+FeiBlueColor@users.noreply.github.com>
2019-08-05 14:33:37 +0800

    Update 11764_Jumping Mario.c

--- 11764_Jumping Mario.c
+++ 11764_Jumping Mario.c
@@ -39,3 +39,53 @@ int main(){
 	
 	return 0;	
 }
+
+
+
+/*2019.08.05 ADD*/
+
+//IMPROVE VERSION
+
+#include <stdio.h>
+
+int main(){
+
+	int t = 0;
+	
+	scanf("%d", &t);
+	
+	int n = 0, i = 0, c = 1;
+	int wallLast, wallNow;
+	
+	while(t != 0){
+	
+		scanf("%d", &n);
+		
+		if(n==1 || n==0){
+			printf("Case %d: 0 0\n", c);
+			c++;
+			t--;
+			continue;
+		}
+		
+		scanf("%d", &wallLast);
+		
+		int high = 0, low = 0;
+		
+		for(i=0; i < n-1; i++){
+			scanf("%d", &wallNow);
+			
+			if(wallLast > wallNow)
+				high++;
+			else if(wallLast < wallNow)
+				low++;
+			wallLast = wallNow;
+		}
+		printf("Case %d: %d %d\n", c, high, low);
+		c++;
+		t--;
+	}
+	
+	return 0;
+}
+
