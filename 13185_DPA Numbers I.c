6d4ea52cd683c4b713bc3fcad1796cd467f0c5ca
FeiBlueColor <50509043+FeiBlueColor@users.noreply.github.com>
2019-08-05 12:31:40 +0800

    Update 13185_DPA Numbers I.c

--- 13185_DPA Numbers I.c
+++ 13185_DPA Numbers I.c
@@ -1,3 +1,4 @@
+/*2019.08.03 finished*/
 #include <stdio.h>
 #include <stdint.h>
 
@@ -14,17 +15,18 @@ int main(){
 		int div[1000]={0}, count=0, res=0;
 		
 		for(i=1; i<=num; i++){
+		//if num % i == 0 means it's a factor
 			if(num%i == 0){
 				div[count] = i;
 				count++;
 				
 			}
 		}
-		
+		//all of them plus together except the number itself
 		for(i=0; i<count-1; i++){
 			res += div[i];
 		}
-		
+		//check whether the sum of factors is > OR < OR == the number
 		if(res > num){
 			printf("abundant\n");
 		}else if(res < num){
