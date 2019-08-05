3506aedcda1762f4f9490ee753acce2ca44176f5
FeiBlueColor <50509043+FeiBlueColor@users.noreply.github.com>
2019-08-05 12:34:09 +0800

    Update 948_Fibonaccimal Base.c

--- 948_Fibonaccimal Base.c
+++ 948_Fibonaccimal Base.c
@@ -1,3 +1,4 @@
+/*2019.08.01 finished*/
 #include <stdio.h>
 #include <stdint.h>
 
@@ -11,6 +12,7 @@ int main(){
 	fib[0]=1;
 	fib[1]=2;
 	
+	//first save each Fibonacci number in array
 	for(i=2; i<40; i++){
 		fib[i]=fib[i-1] + fib[i-2];
 	}
@@ -23,11 +25,12 @@ int main(){
 		
 		printf("%d = ", num);
 		
+		//start from the very last to the first
 		for(i=39; i >= 0; i--){
 			if(num/fib[i] == 1){
 				printf("1");
 				num -= fib[i];
-				flag=1;
+				flag=1; //means can start print 0
 			}else if(flag==1){
 				printf("0");
 			}
