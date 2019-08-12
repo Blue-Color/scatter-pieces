77c98ac8e7a40618eb775bad680e5ff966598e94
FeiBlueColor <50509043+FeiBlueColor@users.noreply.github.com>
2019-08-12 16:58:08 +0800

    Add files via upload

---
+++ 10530_Guessing Game.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+#define MAX(x, y) x>y ? x : y
+#define MIN(x, y) x<y ? x : y
+
+int main(){
+	
+	//num, str "too high" "too low" "right on" -> strcmp, bool
+	int num;
+	char str[10]={0};
+	
+	while(scanf("%d", &num) != EOF && num != 0){
+		
+		int max=10, min=1;
+		
+		while( cin.getline(str, 10) && strcmp(str, "right on") ){
+			
+			if(strcmp(str, "too high") == 0)
+				max = MIN(max, num-1);
+			
+			else if(strcmp(str, "too low") == 0)
+				min = MAX(min, num+1);
+			
+			
+			scanf("%d", &num);
+			
+		}
+		if(num >= min && num <= max)
+			cout << "Stan may be honest\n";
+		else
+			cout << "Stan is dishonest\n";
+	}
+	
+	
+	return 0;
+}
