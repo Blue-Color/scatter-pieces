dba9bcf064b01d900d37a52111d6aae065b1053b
FeiBlueColor <50509043+FeiBlueColor@users.noreply.github.com>
2019-09-07 17:17:57 +0800

    Add files via upload

---
+++ 11988_Broken Keyboard (a.k.a. Beiju Text).cpp
@@ -0,0 +1,33 @@
+#include <list>
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+	string w;
+	int i;
+	
+	while( getline(cin, w) ){
+		
+		list<char> text; 
+		list<char>::iterator it=text.begin();
+	
+		
+		for(i=0; i < w.size(); i++){
+		
+			if(w[i] == '[')
+				it=text.begin();
+			else if (w[i] == ']')
+				it=text.end();
+			else
+				text.insert(it, w[i]);
+		
+		}
+		for(it=text.begin(); it != text.end(); it++){
+			cout << *it;
+		}
+	}
+	
+	return 0;
+}
